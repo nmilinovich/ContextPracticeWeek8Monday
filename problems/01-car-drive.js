9d9d07b576117023145b292ccdcb855a30edf734
Nikola Milinovich <nmilinovich1@gmail.com>
2023-05-22 21:06:50 -0700

    phases 1-3 complete

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,5 +1,14 @@
-// Your code here
-
+class Car {
+	constructor(speed) {
+		//declaring property speed as base 0;
+		this.speed = speed || 0;
+	}
+	drive(newSpeed) {
+		// instance method changes speed to newSpeed and returns the new speed;
+		this.speed = newSpeed;
+		return this.speed;
+	}
+}
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
